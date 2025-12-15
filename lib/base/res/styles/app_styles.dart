876f900901901db4c6ee96a5c01d1456af5c1c9b
Isedin <isedin_k@yahoo.com>
2025-12-15 17:33:15 +0100

    feat: Refactor styles in AppStyles and enhance AllHotels screen layout with HotelGridView

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -4,37 +4,37 @@ Color primary = const Color(0xFF687daf);
 
 class AppStyles {
   static Color primaryColor = primary;
-  static Color bgColor = const Color(0xFFeeedf2);
-  static Color textColor = const Color(0xFF3b3b3b);
-  static Color ticketBlue = const Color(0xFF526799);
-  static Color ticketOrange = const Color(0xFFf37b67);
-  static Color kakiColor = const Color(0xFFf7b259);
-  static Color plainColor = const Color(0xFFBFC2DF);
-  static Color findTicketColor = const Color(0xD91130CE);
-  static Color circleColor = const Color(0xFF189999);
-  static Color ticketColor = const Color(0xFFFFFFFF);
-  static Color dotColor = const Color(0xFF8ACCF7);
-  static Color planeSecondColor = const Color(0xFFBACCF7);
+  static const Color bgColor = Color(0xFFeeedf2);
+  static const Color textColor = Color(0xFF3b3b3b);
+  static const Color ticketBlue = Color(0xFF526799);
+  static const Color ticketOrange = Color(0xFFf37b67);
+  static const Color kakiColor = Color(0xFFf7b259);
+  static const Color plainColor = Color(0xFFBFC2DF);
+  static const Color findTicketColor = Color(0xD91130CE);
+  static const Color circleColor = Color(0xFF189999);
+  static const Color ticketColor = Color(0xFFFFFFFF);
+  static const Color dotColor = Color(0xFF8ACCF7);
+  static const Color planeSecondColor = Color(0xFFBACCF7);
 
-  static TextStyle textStyle = TextStyle(
+  static const TextStyle textStyle = TextStyle(
     fontSize: 16,
     color: textColor,
     fontWeight: FontWeight.w500,
   );
 
-  static TextStyle headlineStyle1 = TextStyle(
+  static const TextStyle headlineStyle1 = TextStyle(
     fontSize: 26,
     fontWeight: FontWeight.bold,
     color: textColor,
   );
 
-  static TextStyle headlineStyle2 = TextStyle(
-    fontSize: 21,
+  static const TextStyle headlineStyle2 = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.bold,
     color: textColor,
   );
 
-  static TextStyle headlineStyle3 = TextStyle(
+  static const TextStyle headlineStyle3 = TextStyle(
     fontSize: 17,
     fontWeight: FontWeight.w500,
   );
@@ -42,5 +42,23 @@ class AppStyles {
     fontSize: 14,
     fontWeight: FontWeight.w500,
     color: Colors.grey.shade500,
+  );
+
+  static const TextStyle cardTitle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: kakiColor,
+  );
+
+  static const TextStyle cardSubtitle = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+    color: bgColor, // ili white, zavisi od pozadine
+  );
+
+  static const TextStyle cardPrice = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    color: kakiColor,
   );
 }
--- lib/screens/home/all_hotels.dart
+++ lib/screens/home/all_hotels.dart
@@ -1,4 +1,5 @@
 import 'package:flight_ticketing_app/base/res/styles/app_styles.dart';
+import 'package:flight_ticketing_app/base/utils/app_json.dart';
 import 'package:flutter/material.dart';
 
 class AllHotels extends StatelessWidget {
@@ -16,16 +17,87 @@ class AllHotels extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
+            crossAxisCount: 2,
             crossAxisSpacing: 16.0,
             mainAxisSpacing: 16.0,
-            childAspectRatio: 1.0,
+            childAspectRatio: 0.85,
           ),
-          itemCount: 20,
+          itemCount: hotelList.length,
           itemBuilder: (context, index) {
-            return Container(child: Text('Hotel $index'), color: Colors.amber);
+            var singleHotel = hotelList[index];
+            return HotelGridView(hotel: singleHotel);
           },
         ),
+      ),
+    );
+  }
+}
+
+class HotelGridView extends StatelessWidget {
+  final Map<String, dynamic> hotel;
+  const HotelGridView({super.key, required this.hotel});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      // width: size.width * 0.6,
+      height: 350,
+      margin: const EdgeInsets.only(right: 8),
+      decoration: BoxDecoration(
+        color: AppStyles.primaryColor,
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AspectRatio(
+            aspectRatio: 1.2,
+            child: Container(
+              // height: 180,
+              decoration: BoxDecoration(
+                color: AppStyles.primaryColor,
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(hotel['image']),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(left: 12.0),
+            child: Text(
+              hotel['name'],
+              style: AppStyles.cardTitle.copyWith(color: AppStyles.kakiColor),
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Text(
+                  hotel['location'],
+                  style: AppStyles.cardSubtitle.copyWith(
+                    color: AppStyles.bgColor,
+                  ),
+                ),
+              ),
+              SizedBox(width: 5),
+              Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: Text(
+                  "${hotel['price_per_night']}€/night",
+                  style: AppStyles.cardPrice.copyWith(
+                    color: AppStyles.kakiColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
